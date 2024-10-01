1ff3e5d22b07a2a812859499e590a4ecd3277d64
devatulparmar <dev.atulparmar@gmail.com>
2024-10-01 19:02:20 +0530

    StatelessWidget, StatefulWidget, OOPs concepts

--- android/app/src/main/kotlin/com/weltec/batch6pm/MainActivity.kt
+++ android/app/src/main/kotlin/com/weltec/batch6pm/MainActivity.kt
@@ -2,4 +2,5 @@ package com.weltec.batch6pm
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,169 +1,155 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  int count = 0;
-
   runApp(
-    MaterialApp(
+   const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: Scaffold(
-        // backgroundColor: Colors.blue.shade900,
-        appBar: AppBar(
-          // backgroundColor: Colors.green,
-          centerTitle: true,
-          elevation: 20,
-          // leadingWidth: 100,
-          leading: const Icon(
-            Icons.arrow_back_ios,
-            color: Colors.yellow,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State createState(){
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.blue.shade900,
+      appBar: AppBar(
+        // backgroundColor: Colors.green,
+        centerTitle: true,
+        elevation: 20,
+        // leadingWidth: 100,
+        leading: const Icon(
+          Icons.arrow_back_ios,
+          color: Colors.yellow,
+        ),
+        title: const Text(
+          "This is AppBar",
+          style: TextStyle(
+            color: Colors.white,
           ),
-          title: const Text(
-            "This is AppBar",
-            style: TextStyle(
-              color: Colors.white,
-            ),
+        ),
+        actions: [
+          Container(
+            width: 10,
           ),
-          actions: [
+          const Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 35,
+          ),
+        ],
+      ),
+      body: Container(
+        color: Colors.grey,
+        child: Column(
+          verticalDirection: VerticalDirection.down,
+          children: [
             Container(
-              width: 10,
+              color: Colors.red,
+              height: 100,
+              width: 100,
+              child: const Center(
+                child: Text('1'),
+              ),
             ),
-            const Icon(
-              Icons.add,
-              color: Colors.white,
-              size: 35,
+            Container(
+              color: Colors.orange,
+              height: 100,
+              width: 100,
+              child: const Center(
+                child: Text('2'),
+              ),
+            ),
+            Container(
+              color: Colors.brown,
+              height: 100,
+              width: 100,
+              child: const Center(
+                child: Text('3'),
+              ),
+            ),
+            Container(
+              color: Colors.pink,
+              height: 100,
+              width: 100,
+              child: const Center(
+                child: Text('4'),
+              ),
+            ),
+            Container(
+              color: Colors.blueAccent,
+              height: 100,
+              width: 100,
+              child: const Center(
+                child: Text('5'),
+              ),
+            ),
+            Container(
+              color: Colors.purple,
+              height: 100,
+              width: 100,
+              child: const Center(
+                child: Text('6'),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [
+                      Colors.red,
+                      Colors.blue,
+                      Colors.black,
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    tileMode: TileMode.repeated),
+              ),
+              child: const Center(
+                child: Text('7'),
+              ),
             ),
           ],
         ),
-        body: Container(
-          color: Colors.grey,
-          child: Wrap(
-            direction: Axis.vertical,
-            crossAxisAlignment: WrapCrossAlignment.end,
-            spacing: 10,
-            runSpacing: 30,
-            verticalDirection: VerticalDirection.down,
-            children: [
-              Container(
-                color: Colors.red,
-                height: 100,
-                width: 100,
-                child: const Center(
-                  child: Text('1'),
-                ),
-              ),
-              Container(
-                color: Colors.orange,
-                height: 100,
-                width: 100,
-                child: const Center(
-                  child: Text('2'),
-                ),
-              ),
-              Container(
-                color: Colors.greenAccent,
-                height: 100,
-                width: 100,
-                child: const Center(
-                  child: Text('3'),
-                ),
-              ),
-              Container(
-                color: Colors.pink,
-                height: 100,
-                width: 100,
-                child: const Center(
-                  child: Text('4'),
-                ),
-              ),
-              Container(
-                color: Colors.blueAccent,
-                height: 100,
-                width: 100,
-                child: const Center(
-                  child: Text('5'),
-                ),
-              ),
-              Container(
-                color: Colors.purple,
-                height: 100,
-                width: 100,
-                child: const Center(
-                  child: Text('6'),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [
-                        Colors.red,
-                        Colors.blue,
-                        Colors.black,
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      tileMode: TileMode.repeated),
-                ),
-                child: const Center(
-                  child: Text('7'),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [
-                        Colors.blue,
-                        Colors.black,
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      tileMode: TileMode.repeated,),
-                ),
-                child: const Center(
-                  child: Text('8'),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [
-                        Colors.blue,
-                        Colors.greenAccent,
-                      ],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      tileMode: TileMode.repeated,),
-                ),
-                child: const Center(
-                  child: Text('9'),
-                ),
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {
-            print('clicked');
-          },
-          label: const Text('This is FLoating Action Button'),
-        ),
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: (){
-        //     print('clicked');
-        //   },
-        //   child: Icon(
-        //     Icons.add,
-        //   ),
-        // ),
-        // floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
       ),
-    ),
-  );
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          print('clicked');
+        },
+        label: const Text('This is FLoating Action Button'),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: (){
+      //     print('clicked');
+      //   },
+      //   child: Icon(
+      //     Icons.add,
+      //   ),
+      // ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+    );
+  }
 }
