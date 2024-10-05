465798928d05653e793bd8ebdfd984810784eac1
devatulparmar <dev.atulparmar@gmail.com>
2024-10-05 19:00:53 +0530

    Expanded widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,32 +18,55 @@ class MyApp extends StatelessWidget {
           title: const Text(
             'This is an Appbar',
             style: TextStyle(
-              color: Colors.white,
-              fontSize: 20.5,
-              fontWeight: FontWeight.normal,
-              letterSpacing: 2,
-              fontStyle: FontStyle.normal,
-              wordSpacing: 5,
-              decoration: TextDecoration.underline,
-              decorationColor: Colors.white,
-              textBaseline: TextBaseline.ideographic
-            ),
+                color: Colors.white,
+                fontSize: 20.5,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3,
+                fontStyle: FontStyle.italic,
+                wordSpacing: 10,
+                decoration: TextDecoration.overline,
+                decorationColor: Colors.white,
+                textBaseline: TextBaseline.alphabetic),
           ),
         ),
-        body: const Text(
-          'HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello',
-          style: TextStyle(
-            color: Colors.green,
-            fontSize: 20.5,
-            fontWeight: FontWeight.normal,
-            letterSpacing: 2,
-            fontStyle: FontStyle.normal,
-            wordSpacing: 5,
-            decoration: TextDecoration.underline,
-            decorationColor: Colors.green,
-            textBaseline: TextBaseline.alphabetic,
-            overflow: TextOverflow.fade,
-          ),
+        body: Column(
+          children: [
+            const Row(
+              children: [
+                Expanded(
+                  flex: 0,
+                  child: Text("Hello"),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Text("Hello"),
+                ),
+                Expanded(
+                  flex: 0,
+                  child: Text("Hello"),
+                ),
+              ],
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.red,
+                width: 100,
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              height: 500,
+              width: 100,
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.yellow,
+                width: 100,
+              ),
+            ),
+          ],
         ),
       ),
     );
