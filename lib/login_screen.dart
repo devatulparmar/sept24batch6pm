8d38d3a17e771670bcda26f0785bbc32fb2beccf
devatulparmar <dev.atulparmar@gmail.com>
2024-11-07 19:00:55 +0530

    SnackBar, TextFormField, Email Password validator, Form, GlobalKey

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,8 @@
 import 'package:batch6pm/utils/const.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -17,6 +19,8 @@ class _LoginScreenState extends State<LoginScreen> {
   FocusNode emailFocusNode = FocusNode();
   FocusNode passwordFocusNode = FocusNode();
 
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   void dispose() {
     super.dispose();
@@ -32,211 +36,242 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            const SizedBox(height: 40),
-            TextField(
-              controller: emailController,
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.emailAddress,
-              focusNode: emailFocusNode,
-              decoration: const InputDecoration(
-                hintText: 'Email',
-                enabledBorder: enabledBorder,
-                focusedBorder: focusedBorder,
+        child: Form(
+          autovalidateMode: AutovalidateMode.disabled,
+          key: formKey,
+          child: Column(
+            children: [
+              const SizedBox(height: 40),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: emailController,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.emailAddress,
+                focusNode: emailFocusNode,
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                  border: enabledBorder,
+                  enabledBorder: enabledBorder,
+                  focusedBorder: focusedBorder,
+                ),
+                onTapOutside: (PointerDownEvent event) {
+                  emailFocusNode.unfocus();
+                },
+                validator: (String? value) =>
+                    MyValidators.emailValidator(value),
               ),
-              onTapOutside: (PointerDownEvent event){
-                emailFocusNode.unfocus();
-              },
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: passwordController,
-              textInputAction: TextInputAction.done,
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: isObscureText,
-              focusNode: passwordFocusNode,
-              decoration: InputDecoration(
-                hintText: 'Password',
-                enabledBorder: enabledBorder,
-                focusedBorder: focusedBorder,
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    if (isObscureText == true) {
-                      isObscureText = false;
-                    } else {
-                      isObscureText = true;
-                    }
-                    setState(() {});
-                  },
-                  icon: Icon(
-                    isObscureText
-                        ? Icons.remove_red_eye_outlined
-                        : Icons.remove_red_eye,
-                    color: isObscureText ? Colors.grey : Colors.green,
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: passwordController,
+                textInputAction: TextInputAction.done,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: isObscureText,
+                focusNode: passwordFocusNode,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  border: enabledBorder,
+                  enabledBorder: enabledBorder,
+                  focusedBorder: focusedBorder,
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      if (isObscureText == true) {
+                        isObscureText = false;
+                      } else {
+                        isObscureText = true;
+                      }
+                      setState(() {});
+                    },
+                    icon: Icon(
+                      isObscureText
+                          ? Icons.remove_red_eye_outlined
+                          : Icons.remove_red_eye,
+                      color: isObscureText ? Colors.grey : Colors.green,
+                    ),
                   ),
                 ),
+                onTapOutside: (PointerDownEvent event) {
+                  passwordFocusNode.unfocus();
+                },
+                validator: (String? value) =>
+                    MyValidators.passwordValidator(value),
               ),
-              onTapOutside: (PointerDownEvent event){
-                passwordFocusNode.unfocus();
-              },
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print('Elevated button click');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                    ),
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(
-                        color: Colors.white,
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print('Elevated button click');
+                        if (formKey.currentState!.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: const Text('Login Success'),
+                              backgroundColor: Colors.green,
+                              duration: const Duration(seconds: 10),
+                              // showCloseIcon: true,
+                              // closeIconColor: Colors.red,
+                              action: SnackBarAction(
+                                onPressed: (){
+                                  print('Hello');
+                                },
+                                label: "Click Here",
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(35),
+                              ),
+                              behavior: SnackBarBehavior.floating,
+                            ),
+                          );
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                      ),
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: () {
-                print('forget password click');
-              },
-              child: const Text('Forget Password'),
-            ),
-
-            /*
-           TextField(
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                focusColor: Colors.red,
-                fillColor: Colors.green,
-                focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.green,
-                    width: 2,
-                  ),
-                ),
-                // suffixIcon: IconButton(
-                //   onPressed: () {
-                //     emailController.clear();
-                //   },
-                //   icon: const Icon(
-                //     Icons.clear,
-                //     color: Colors.red,
-                //   ),
-                // ),
+                ],
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: passwordController,
-              style: const TextStyle(fontSize: 16, color: Colors.red),
-              textDirection: TextDirection.ltr,
-              autocorrect: true,
-              autofocus: true,
-              cursorColor: Colors.red,
-              cursorHeight: 20,
-              cursorWidth: 10,
-              cursorRadius: const Radius.circular(35),
-              // showCursor: false,
-              cursorOpacityAnimates: true,
-              // maxLength: 10,
-              // maxLines: 5,
-              // minLines: 2,
-              // readOnly: true,
-              // textAlign: TextAlign.center,
-              textAlignVertical: TextAlignVertical.center,
-              textCapitalization: TextCapitalization.words,
-              textInputAction: TextInputAction.search,
-              keyboardType: TextInputType.name,
-              keyboardAppearance: Brightness.dark,
-              obscureText: isObscureText,
-              onChanged: (String value){
-                print(value);
-                setState(() {});
-              },
-              // obscuringCharacter: "*",
-              // enabled: false,
-              decoration: InputDecoration(
-                // border: OutlineInputBorder(
-                //   borderSide: const BorderSide(
-                //     color: Colors.blue,
-                //     width: 2,
-                //   ),
-                //   borderRadius: BorderRadius.circular(35),
-                // ),
-                focusColor: Colors.red,
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.green,
-                    width: 2,
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: () {
+                  print('forget password click');
+                },
+                child: const Text('Forget Password'),
+              ),
+              /*
+             TextField(
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  focusColor: Colors.red,
+                  fillColor: Colors.green,
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
                   ),
-                  borderRadius: BorderRadius.circular(35),
+                  // suffixIcon: IconButton(
+                  //   onPressed: () {
+                  //     emailController.clear();
+                  //   },
+                  //   icon: const Icon(
+                  //     Icons.clear,
+                  //     color: Colors.red,
+                  //   ),
+                  // ),
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.blue,
-                    width: 2,
-                  ),
-                  borderRadius: BorderRadius.circular(35),
-                ),
-                disabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.grey,
-                    width: 5,
-                  ),
-                  borderRadius: BorderRadius.circular(35),
-                ),
-                hintText: "Enter Password",
-                labelText: "Enter Password",
-                hintStyle: const TextStyle(fontSize: 12, color: Colors.blue),
-                // label: const Icon(Icons.password),
-                labelStyle: const TextStyle(fontSize: 12, color: Colors.blue),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: passwordController,
+                style: const TextStyle(fontSize: 16, color: Colors.red),
+                textDirection: TextDirection.ltr,
+                autocorrect: true,
+                autofocus: true,
+                cursorColor: Colors.red,
+                cursorHeight: 20,
+                cursorWidth: 10,
+                cursorRadius: const Radius.circular(35),
+                // showCursor: false,
+                cursorOpacityAnimates: true,
+                // maxLength: 10,
+                // maxLines: 5,
+                // minLines: 2,
+                // readOnly: true,
+                // textAlign: TextAlign.center,
+                textAlignVertical: TextAlignVertical.center,
+                textCapitalization: TextCapitalization.words,
+                textInputAction: TextInputAction.search,
+                keyboardType: TextInputType.name,
+                keyboardAppearance: Brightness.dark,
+                obscureText: isObscureText,
+                onChanged: (String value){
+                  print(value);
+                  setState(() {});
+                },
+                // obscuringCharacter: "*",
                 // enabled: false,
-                contentPadding: const EdgeInsets.symmetric(horizontal: 15),
-                helperText: "Here is Helper Text",
-                // icon: const Icon(Icons.password),
-                // iconColor: Colors.red,
-                // prefix shows only when focus
-                // prefix: const Icon(Icons.password),
-                prefixIcon: const Icon(
-                  Icons.email,
-                  color: Colors.pink,
-                ),
-                // prefixIconColor: Colors.red,
-                // prefixText: "Prefix Text",
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    if (isObscureText == true) {
-                      isObscureText = false;
-                    }
-                    else {
-                      isObscureText = true;
-                    }
-                    setState(() {});
-                  },
-                  icon: Icon(
-                    isObscureText
-                        ? Icons.remove_red_eye_outlined
-                        : Icons.remove_red_eye,
-                    color: isObscureText ? Colors.grey : Colors.pink,
+                decoration: InputDecoration(
+                  // border: OutlineInputBorder(
+                  //   borderSide: const BorderSide(
+                  //     color: Colors.blue,
+                  //     width: 2,
+                  //   ),
+                  //   borderRadius: BorderRadius.circular(35),
+                  // ),
+                  focusColor: Colors.red,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(35),
                   ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.blue,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(35),
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      color: Colors.grey,
+                      width: 5,
+                    ),
+                    borderRadius: BorderRadius.circular(35),
+                  ),
+                  hintText: "Enter Password",
+                  labelText: "Enter Password",
+                  hintStyle: const TextStyle(fontSize: 12, color: Colors.blue),
+                  // label: const Icon(Icons.password),
+                  labelStyle: const TextStyle(fontSize: 12, color: Colors.blue),
+                  // enabled: false,
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 15),
+                  helperText: "Here is Helper Text",
+                  // icon: const Icon(Icons.password),
+                  // iconColor: Colors.red,
+                  // prefix shows only when focus
+                  // prefix: const Icon(Icons.password),
+                  prefixIcon: const Icon(
+                    Icons.email,
+                    color: Colors.pink,
+                  ),
+                  // prefixIconColor: Colors.red,
+                  // prefixText: "Prefix Text",
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      if (isObscureText == true) {
+                        isObscureText = false;
+                      }
+                      else {
+                        isObscureText = true;
+                      }
+                      setState(() {});
+                    },
+                    icon: Icon(
+                      isObscureText
+                          ? Icons.remove_red_eye_outlined
+                          : Icons.remove_red_eye,
+                      color: isObscureText ? Colors.grey : Colors.pink,
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Colors.orange.shade50,
                 ),
-                filled: true,
-                fillColor: Colors.orange.shade50,
               ),
-            ),
-            Text(passwordController.text),*/
-          ],
+              Text(passwordController.text),*/
+            ],
+          ),
         ),
       ),
     );
--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -13,3 +13,42 @@ const focusedBorder = OutlineInputBorder(
     width: 2,
   ),
 );
+
+
+class MyValidators {
+  static emailValidator(value) {
+    if (value == null || value == '') {
+      return "Enter an Email";
+    }
+    else if (!RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value)) {
+      return 'Enter a valid email';
+    }
+    else {
+      return null;
+    }
+  }
+
+  static passwordValidator(value) {
+    if (value == null || value == '') {
+      return "Enter a password";
+    }
+    else if (!RegExp(r'^(?=.*?[A-Z])').hasMatch(value)) {
+      return "Should contain at least one upper case";
+    }
+    else if (!RegExp(r'^(?=.*[a-z])').hasMatch(value)) {
+      return "Should contain at least one lower case";
+    }
+    else if (!RegExp(r'^(?=.*?[0-9])').hasMatch(value)) {
+      return "Should contain at least one digit";
+    }
+    else if (!RegExp(r'^(?=.*?[!@#\$&*~])').hasMatch(value)) {
+      return "Should contain at least one Special character";
+    }
+    else if (!RegExp(r'^.{6,}').hasMatch(value)) {
+      return "Must be at least 6 characters in length ";
+    } else {
+      return null;
+    }
+  }
+}
